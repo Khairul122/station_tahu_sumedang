f2ecee441d7b538ca09451476e5f51d519a90536
Khairul122 <khairulhuda242@gmail.com>
2025-07-28 16:07:05 +0700

    Refactor sidebar navigation to use a dynamic menu rendering function

--- template/sidebar.php
+++ template/sidebar.php
@@ -1,121 +1,61 @@
 <?php
-$userRole = $_SESSION['role'] ?? 'guest';
+$userRole   = $_SESSION['role'] ?? 'guest';
 $isLoggedIn = isset($_SESSION['logged_in']) && $_SESSION['logged_in'] === true;
+
+function renderNavItem($controller, $action, $icon, $title)
+{
+  $url = "?controller={$controller}" . ($action ? "&action={$action}" : "");
+  echo "
+    <li class='nav-item'>
+      <a class='nav-link' href='{$url}'>
+        <i class='mdi {$icon} menu-icon'></i>
+        <span class='menu-title'>{$title}</span>
+      </a>
+    </li>";
+}
+
+$menus = [
+  'admin' => [
+    ['Store', 'index', 'mdi-package-variant', 'Store'],
+    ['produk', 'index', 'mdi-package-variant', 'Data Produk'],
+    ['customer', 'index', 'mdi-account-group', 'Data Customer'],
+    ['membership', 'index', 'mdi-card-account-details', 'Data Membership'],
+    ['transaksiadmin', 'index', 'mdi-clipboard-text', 'Data Transaksi'],
+    ['adminreward', 'index', 'mdi-gift', 'Kelola Reward'],
+    ['datamember', 'index', 'mdi-account', 'Data Member']
+  ],
+  'pimpinan' => [
+    ['laporan', 'index', 'mdi-file-document', 'Laporan']
+  ],
+  'manajer' => [
+    ['produk', 'index', 'mdi-package-variant', 'Data Produk'],
+    ['transaksiadmin', 'index', 'mdi-clipboard-text', 'Data Transaksi']
+  ],
+  'member' => [
+    ['pembelian', 'index', 'mdi-cart', 'Pembelian'],
+    ['pembelian', 'history', 'mdi-history', 'Riwayat Pembelian'],
+    ['reward', 'index', 'mdi-gift-outline', 'Klaim Reward'],
+    ['member', 'index', 'mdi-account-multiple', 'Profile']
+  ]
+];
 ?>
 
 <nav class="sidebar sidebar-offcanvas" id="sidebar">
   <ul class="nav">
-    
+
     <?php if ($isLoggedIn): ?>
-      <li class="nav-item">
-        <a class="nav-link" href="?controller=dashboard">
-          <i class="mdi mdi-view-dashboard menu-icon"></i>
-          <span class="menu-title">Dashboard</span>
-        </a>
-      </li>
+      <?php renderNavItem('dashboard', '', 'mdi-view-dashboard', 'Dashboard'); ?>
 
-      <?php if ($userRole === 'admin' || $userRole === 'manajer'): ?>
-          <li class="nav-item">
-          <a class="nav-link" href="?controller=Store&action=index">
-            <i class="mdi mdi-package-variant menu-icon"></i>
-            <span class="menu-title">Store</span>
-          </a>
-        </li>
-
-        <li class="nav-item">
-          <a class="nav-link" href="?controller=produk&action=index">
-            <i class="mdi mdi-package-variant menu-icon"></i>
-            <span class="menu-title">Data Produk</span>
-          </a>
-        </li>
-
-        <li class="nav-item">
-          <a class="nav-link" href="?controller=customer&action=index">
-            <i class="mdi mdi-account-group menu-icon"></i>
-            <span class="menu-title">Data Customer</span>
-          </a>
-        </li>
-
-        <li class="nav-item">
-          <a class="nav-link" href="?controller=membership&action=index">
-            <i class="mdi mdi-card-account-details menu-icon"></i>
-            <span class="menu-title">Data Membership</span>
-          </a>
-        </li>
-
-        <li class="nav-item">
-          <a class="nav-link" href="?controller=transaksiadmin&action=index">
-            <i class="mdi mdi-clipboard-text menu-icon"></i>
-            <span class="menu-title">Data Transaksi</span>
-          </a>
-        </li>
-
-        <li class="nav-item">
-          <a class="nav-link" href="?controller=adminreward&action=index">
-            <i class="mdi mdi-gift menu-icon"></i>
-            <span class="menu-title">Kelola Reward</span>
-          </a>
-        </li>
-
-        <li class="nav-item">
-          <a class="nav-link" href="?controller=datamember&action=index">
-            <i class="mdi mdi-account menu-icon"></i>
-            <span class="menu-title">Data Member</span>
-          </a>
-        </li>
-
-      <?php elseif ($userRole === 'pimpinan'): ?>
-        <li class="nav-item">
-          <a class="nav-link" href="?controller=laporan&action=index">
-            <i class="mdi mdi-file-document menu-icon"></i>
-            <span class="menu-title">Laporan</span>
-          </a>
-        </li>
-
-      <?php elseif ($userRole === 'member'): ?>
-        <li class="nav-item">
-          <a class="nav-link" href="?controller=pembelian&action=index">
-            <i class="mdi mdi-cart menu-icon"></i>
-            <span class="menu-title">Pembelian</span>
-          </a>
-        </li>
-
-        <li class="nav-item">
-          <a class="nav-link" href="?controller=pembelian&action=history">
-            <i class="mdi mdi-history menu-icon"></i>
-            <span class="menu-title">Riwayat Pembelian</span>
-          </a>
-        </li>
-
-        <li class="nav-item">
-          <a class="nav-link" href="?controller=reward&action=index">
-            <i class="mdi mdi-gift-outline menu-icon"></i>
-            <span class="menu-title">Klaim Reward</span>
-          </a>
-        </li>
-
-        <li class="nav-item">
-          <a class="nav-link" href="?controller=member&action=index">
-            <i class="mdi mdi-account-multiple menu-icon"></i>
-            <span class="menu-title">Profile</span>
-          </a>
-        </li>
+      <?php if (isset($menus[$userRole])): ?>
+        <?php foreach ($menus[$userRole] as $menu): ?>
+          <?php renderNavItem($menu[0], $menu[1], $menu[2], $menu[3]); ?>
+        <?php endforeach; ?>
       <?php endif; ?>
 
-      <li class="nav-item">
-        <a class="nav-link" href="?controller=auth&action=logout" onclick="return confirm('Yakin ingin logout?')">
-          <i class="mdi mdi-power menu-icon"></i>
-          <span class="menu-title">Logout</span>
-        </a>
-      </li>
+      <?php renderNavItem('auth', 'logout', 'mdi-power', 'Logout'); ?>
 
     <?php else: ?>
-      <li class="nav-item">
-        <a class="nav-link" href="?controller=auth&action=login">
-          <i class="mdi mdi-login menu-icon"></i>
-          <span class="menu-title">Login</span>
-        </a>
-      </li>
+      <?php renderNavItem('auth', 'login', 'mdi-login', 'Login'); ?>
     <?php endif; ?>
 
   </ul>
